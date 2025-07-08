98c023ef2e77db0173c9786032122946fc9849e0
Hyperag⬡n <23475800+hyperagon@users.noreply.github.com>
2025-07-08 20:05:44 +0100

    Update text_to_art.py

--- text_to_art.py
+++ text_to_art.py
@@ -1,7 +1,168 @@
-# Vesion 0.2
-# python text-to-art.py "a b c d e f h i j k l m n o p q r s t u b w x y z"
+# Vesion 0.6, 5x5
+# python text-to-art.py "a b c d e f g h i j k l m n o p q r s t u v w x y z"
 
 import sys
+
+ART = {
+" ":["  ", "  ", "  ", "  ", "  "],
+"A":[
+"╔═══╗",
+"║   ║",
+"╠═══╣",
+"║   ║",
+"║   ║",
+],"B":[
+"╔═══╗",
+"║  ╔╝",
+"╠══╣ ",
+"║  ╚╗",
+"╚═══╝",
+],"C":[
+"╔════",
+"║    ",
+"║    ",
+"║    ",
+"╚════",
+],"D":[
+"╔══╗ ",
+"║  ╚╗",
+"║   ║",
+"║  ╔╝",
+"╚══╝ ",
+],"E":[
+"╔════",
+"║    ",
+"╠═══ ",
+"║    ",
+"╚════",
+],"F":[
+"╔════",
+"║    ",
+"╠═══ ",
+"║    ",
+"║    ",
+],"G":[
+"╔═══ ",
+"║    ",
+"║ ══╗",
+"║   ║",
+"╚═══╝",
+],"H":[
+"║   ║",
+"║   ║",
+"╠═══╣",
+"║   ║",
+"║   ║",
+],"I":[
+"══╦══",
+"  ║  ",
+"  ║  ",
+"  ║  ",
+"══╩══",
+],"J":[
+"    ║",
+"    ║",
+"    ║",
+"   ╔╝",
+"═══╝ ",
+],"K":[
+"║   ║",
+"║╔══╝",
+"╠╣   ",
+"║╚══╗",
+"║   ║",
+],"L":[
+"║    ",
+"║    ",
+"║    ",
+"║    ",
+"╚════",
+],"M":[
+"   ",
+"   ",
+"   ",
+"   ",
+"   ",
+],"N":[
+"   ",
+"   ",
+"   ",
+"   ",
+"   ",
+],"O":[
+"╔═══╗",
+"║   ║",
+"║   ║",
+"║   ║",
+"╚═══╝",
+],"P":[
+"   ",
+"   ",
+"   ",
+"   ",
+"   ",
+],"Q":[
+"╔══╗ ",
+"║  ║ ",
+"║  ║ ",
+"╚══╣ ",
+"   ╚═",
+],"R":[
+"   ",
+"   ",
+"   ",
+"   ",
+"   ",
+],"S":[
+"╔════",
+"║    ",
+"╚═══╗",
+"    ║",
+"════╝",
+],"T":[
+"══╦══",
+"  ║  ",
+"  ║  ",
+"  ║  ",
+"  ║  ",
+],"U":[
+"║   ║",
+"║   ║",
+"║   ║",
+"║   ║",
+"╚═══╝",
+],"V":[
+"   ",
+"   ",
+"   ",
+"   ",
+"   ",
+],"W":[
+"   ",
+"   ",
+"   ",
+"   ",
+"   ",
+],"X":[
+"   ",
+"   ",
+"   ",
+"   ",
+"   ",
+],"Y":[
+"   ",
+"   ",
+"   ",
+"   ",
+"   ",
+],"Z":[
+"   ",
+"   ",
+"   ",
+"   ",
+"   ",
+],
+}
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -12,167 +173,14 @@ if __name__ == '__main__':
         letter = 0
         art = ["", "", "", "", ""]
         while letter < len(sequence):
-            if sequence[letter].upper() == ' ':
-                art[0] += " "
-                art[1] += " "
-                art[2] += " "
-                art[3] += " "
-                art[4] += " "
-            elif sequence[letter].upper() == 'A':
-                art[0] += "⬛️⬛️⬛️"
-                art[1] += "⬛️ ⬛️"
-                art[2] += "⬛️⬛️⬛️"
-                art[3] += "⬛️ ⬛️"
-                art[4] += "⬛️ ⬛️"
-            elif sequence[letter].upper() == 'B':
-                art[0] += "⬛️⬛️ "
-                art[1] += "⬛️ ⬛️"
-                art[2] += "⬛️⬛️ "
-                art[3] += "⬛️ ⬛️"
-                art[4] += "⬛️⬛️ "
-            elif sequence[letter].upper() == 'C':
-                art[0] += "⬛️⬛️⬛️"
-                art[1] += "⬛️   "
-                art[2] += "⬛️   "
-                art[3] += "⬛️   "
-                art[4] += "⬛️⬛️⬛️"
-            elif sequence[letter].upper() == 'D':
-                art[0] += "⬛️⬛️ "
-                art[1] += "⬛️ ⬛️"
-                art[2] += "⬛️ ⬛️"
-                art[3] += "⬛️ ⬛️"
-                art[4] += "⬛️⬛️ "
-            elif sequence[letter].upper() == 'E':
-                art[0] += "⬛️⬛️⬛️"
-                art[1] += "⬛️  "
-                art[2] += "⬛️⬛️⬛️"
-                art[3] += "⬛️  "
-                art[4] += "⬛️⬛️⬛️"
-            elif sequence[letter].upper() == 'F':
-                art[0] += "⬛️⬛️⬛️"
-                art[1] += "⬛️  "
-                art[2] += "⬛️⬛️⬛️"
-                art[3] += "⬛️  "
-                art[4] += "⬛️  "
-            elif sequence[letter].upper() == 'G':
-                art[0] += "⬛️⬛️⬛️⬛️"
-                art[1] += "⬛️   "
-                art[2] += "⬛️ ⬛️⬛️"
-                art[3] += "⬛️  ⬛️"
-                art[4] += "⬛️⬛️⬛️⬛️"
-            elif sequence[letter].upper() == 'H':
-                art[0] += "⬛️ ⬛️"
-                art[1] += "⬛️ ⬛️"
-                art[2] += "⬛️⬛️⬛️"
-                art[3] += "⬛️ ⬛️"
-                art[4] += "⬛️ ⬛️"
-            elif sequence[letter].upper() == 'I':
-                art[0] += "⬛️⬛️⬛️"
-                art[1] += " ⬛️ "
-                art[2] += " ⬛️ "
-                art[3] += " ⬛️ "
-                art[4] += "⬛️⬛️⬛️"
-            elif sequence[letter].upper() == 'J':
-                art[0] += "⬛️⬛️⬛️"
-                art[1] += "  ⬛️"
-                art[2] += "  ⬛️"
-                art[3] += "  ⬛️"
-                art[4] += "⬛️⬛️ "
-            elif sequence[letter].upper() == 'K':
-                art[0] += "⬛️ ⬛️"
-                art[1] += "⬛️⬛️ "
-                art[2] += "⬛️  "
-                art[3] += "⬛️⬛️ "
-                art[4] += "⬛️ ⬛️"
-            elif sequence[letter].upper() == 'L':
-                art[0] += "⬛️  "
-                art[1] += "⬛️  "
-                art[2] += "⬛️  "
-                art[3] += "⬛️  "
-                art[4] += "⬛️⬛️⬛️"
-            elif sequence[letter].upper() == 'M':
-                art[0] += "⬛️   ⬛️"
-                art[2] += "⬛️ ⬛️ ⬛️"
-                art[3] += "⬛️   ⬛️"
-                art[4] += "⬛️   ⬛️"
-            elif sequence[letter].upper() == 'N':
-                art[0] += "⬛️   ⬛️"
-                art[1] += "⬛️⬛️  ⬛️"
-                art[2] += "⬛️ ⬛️ ⬛️"
-                art[3] += "⬛️  ⬛️⬛️"
-                art[4] += "⬛️   ⬛️"
-            elif sequence[letter].upper() == 'O':
-                art[0] += "⬛️⬛️⬛️"
-                art[1] += "⬛️ ⬛️"
-                art[2] += "⬛️ ⬛️"
-                art[3] += "⬛️ ⬛️"
-                art[4] += "⬛️⬛️⬛️"
-            elif sequence[letter].upper() == 'P':
-                art[0] += "⬛️⬛️⬛️"
-                art[1] += "⬛️ ⬛️"
-                art[2] += "⬛️⬛️⬛️"
-                art[3] += "⬛️  "
-                art[4] += "⬛️  "
-            elif sequence[letter].upper() == 'Q':
-                art[0] += "⬛️⬛️⬛️ "
-                art[1] += "⬛️ ⬛️ "
-                art[2] += "⬛️ ⬛️ "
-                art[3] += "⬛️ ⬛️ "
-                art[4] += "⬛️⬛️⬛️⬛️"
-            elif sequence[letter].upper() == 'R':
-                art[0] += "⬛️⬛️⬛️"
-                art[1] += "⬛️ ⬛️"
-                art[2] += "⬛️ ⬛️"
-                art[3] += "⬛️⬛️ "
-                art[4] += "⬛️ ⬛️"
-            elif sequence[letter].upper() == 'S':
-                art[0] += "⬛️⬛️⬛️"
-                art[1] += "⬛️  "
-                art[2] += "⬛️⬛️⬛️"
-                art[3] += "  ⬛️"
-                art[4] += "⬛️⬛️⬛️"
-            elif sequence[letter].upper() == 'T':
-                art[0] += "⬛️⬛️⬛️"
-                art[1] += " ⬛️ "
-                art[2] += " ⬛️ "
-                art[3] += " ⬛️ "
-                art[4] += " ⬛️ "
-            elif sequence[letter].upper() == 'U':
-                art[0] += "⬛️ ⬛️"
-                art[1] += "⬛️ ⬛️"
-                art[2] += "⬛️ ⬛️"
-                art[3] += "⬛️ ⬛️"
-                art[4] += "⬛️⬛️⬛️"
-            elif sequence[letter].upper() == 'V':
-                art[0] += "⬛️   ⬛️"
-                art[1] += "⬛️   ⬛️"
-                art[2] += " ⬛️ ⬛️ "
-                art[3] += " ⬛️ ⬛️ "
-                art[4] += "  ⬛️  "
-            elif sequence[letter].upper() == 'W':
-                art[0] += "⬛️   ⬛️"
-                art[1] += "⬛️   ⬛️"
-                art[2] += "⬛️   ⬛️"
-                art[3] += "⬛️ ⬛️ ⬛️"
-                art[4] += "  ⬛️ ⬛️  "
-            elif sequence[letter].upper() == 'X':
-                art[0] += "⬛️   ⬛️"
-                art[1] += " ⬛️ ⬛️ "
-                art[2] += "  ⬛️  "
-                art[3] += " ⬛️ ⬛️ "
-                art[4] += "⬛️   ⬛️"
-            elif sequence[letter].upper() == 'Y':
-                art[0] += "⬛️   ⬛️"
-                art[1] += " ⬛️ ⬛️ "
-                art[2] += "  ⬛️  "
-                art[3] += "  ⬛️  "
-                art[4] += "  ⬛️  "
-            elif sequence[letter].upper() == 'Z':
-                art[0] += "⬛️⬛️⬛️⬛️"
-                art[1] += "   ⬛️"
-                art[2] += " ⬛️⬛️  "
-                art[3] += "⬛️    "
-                art[4] += "⬛️⬛️⬛️⬛️⬛️"
+            target = sequence[letter].upper()
+            print(target)
+            if target in ART:
+                art[0] += ART[target][0]
+                art[1] += ART[target][1]
+                art[2] += ART[target][2]
+                art[3] += ART[target][3]
+                art[4] += ART[target][4]
             letter += 1
             
         print("```")
