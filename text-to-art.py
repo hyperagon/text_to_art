3724364f632733941d19760712dce951b85aeaf1
Hyperag⬡n <23475800+hyperagon@users.noreply.github.com>
2025-07-21 10:09:11 +0100

    Add files via upload

--- text-to-art.py
+++ text-to-art.py
@@ -1,4 +1,4 @@
-# Vesion 0.9, 5x5
+# Vesion 1.0, 5x5
 # python text-to-art.py "a b c d e f g h i j k l m"
 # python text-to-art.py "n o p q r s t u v w x y z"
 # python text-to-art.py "1 2 3 4 5 6 7 8 9 0"
@@ -223,6 +223,18 @@ ART = {
 "║╔╝║",
 "╠╝ ║",
 "╚══╝",
+],"+":[
+"     ",
+"  ║  ",
+"══║══",
+"  ║  ",
+"     ",
+],"-":[
+"     ",
+"     ",
+"═════",
+"     ",
+"     ",
 ]
 }
 
